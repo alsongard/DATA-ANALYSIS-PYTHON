08a192ee52f1053ff6efb5649d63fcb1d3302f54
alsongard <alsongard@gmail.com>
2023-12-14 04:25:39 -0500

    storeSystemProto

---
+++ groceryStore.py
@@ -0,0 +1,21 @@
+"""
+bag of  ice = 1.25;
+20% profit
+500 bags of ice sells
+
+calculate profit
+"""
+price = 1.25
+print("the price of a bag is {}".format(price))
+profit_per_Bag = ((20/100) * price)
+print("Profit per bag is {}".format(profit_per_Bag))
+bags = 500
+print("Number of bags sold to day is {}".format(bags))
+total = bags * price
+print("the total price of {} bags is : {}".format(bags, total))
+newProfit = profit_per_Bag * bags
+print("total profit made from selling 500 bags is {}".format(newProfit))
+
+
+
+##market system for calculating profits, analysing sells in an organization and draw charts
