31ffe0eddd92b05f719e0fcfc0b943195095b3a8
alsongard <alsongard@gmail.com>
2023-12-14 15:59:55 -0500

    typesMethodsMultiline

---
+++ stringMethods.py
@@ -0,0 +1,29 @@
+"""
+    this is a file to show all tutorials for string methdos
+    string methods
+    upper(), capitalize(), lower(), split()
+
+    string name or a variable name
+
+    "monday".upper()
+    "monday".capitalize()
+    "MONDAY".lower()
+    "MONDAY".replace("mon", "tues")
+    instead of a string use a variable name
+    myvar.method()
+"""
+
+#capture the day name of the day from the user
+print("User the following methods used in string data types\n\tupper()\n\tcapitalizae()\n\tlower()\n\treplace()\n\tsplit()")
+print("Use the replace keyword to remove the data in the variable 'day' ")
+
+day = input("Enter the day of the day : \n")
+print("day entered by the user is {}".format(day))
+print("to replace the day entered we use the replace() method, changed rom {} to {}".format(day, day.replace(day, "wednesday")))
+myFruits = "watermelon, lemon, banana, apple, carrot, tomatoes, mangoes"
+music = input("Enter your favourite music : \n")
+print("favourite music of user is {} and applying upper() methods yields {}".format(music, music.upper()))
+print("favourite music of user is {} and applying capitalize() methods yields {}".format(music, music.capitalize()))
+print("favourite music of user is {} and applying lower() methods yields {}".format(music, music.lower()))
+print("the string will be converted to a list below :\n  {}".format(myFruits))
+print("converted : \n {}".format(myFruits.split(",")))
