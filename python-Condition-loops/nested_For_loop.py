32fb40bba0ce7e94f7cc9d15eac70600f9c54b30
alsongard <alsongard@gmail.com>
2023-12-16 21:02:11 -0500

    nestedLoop

---
+++ python-Condition-loops/nested_For_loop.py
@@ -0,0 +1,28 @@
+"""
+    in a nested for loop, useful for looping in a list of a list or a sequence of a sequence
+"""
+
+myList = [ {"name": "gard", "age":23, "gender":"male"}, {"name": "alson", "age":23, "gender":"male"}]
+
+for item in myList:
+    for key in item:
+        print("the key is {} and value is {}".format(key, item[key]))
+
+
+months = ["January", "February", "March"]
+
+for item in months:
+    for day in item:
+        if item == "January":
+            for i in range(32):
+                day = i
+        elif item == "February":
+            for i in range(28):
+                day = i 
+        else:
+            for i in range(31):
+                day = i 
+
+    print("the month is {} and days are {}".format(item, day))
+
+print(type(day))
