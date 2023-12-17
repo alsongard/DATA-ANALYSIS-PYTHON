523977c7158a9b6bd326f47ac5f24b0ed4c779d2
alsongard <alsongard@gmail.com>
2023-12-17 09:27:34 -0500

    problem2

--- python-Exercise/assignment1.py
+++ python-Exercise/assignment1.py
@@ -15,3 +15,16 @@ print("{} is aged {}, and owns an {}\n".format(person["name"], person["age"], "a
 
 for key in person:
     print("Key : {} && Value : {}\n".format(key,person[key]))
+
+
+"""
+    assignment 1 problem 2
+"""
+
+myList = ["black", "blue", "darkBlue", 5, True]
+        # index 0 ,  1,     2,        , 3, 4 total of 5 elements
+print("the length of myList is {}".format(len(myList)))
+print(myList)
+
+print("My favourite color is {}".format(myList[0:3]))
+print("Number of pets i have are {}".format(myList[3]))
