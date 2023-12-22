631840ddda03db8f0be57d41dc45ed0d41d1c9b2
alsongard <alsongard@gmail.com>
2023-12-22 02:11:58 -0500

    majorFunctions

--- python-OS-interaction/os-Interaction.py
+++ python-OS-interaction/os-Interaction.py
@@ -10,4 +10,21 @@ url1 = 'https://gist.githubusercontent.com/aakashns/257f6e6c8719c17d0e498ea287d1
 url2 = 'https://gist.githubusercontent.com/aakashns/257f6e6c8719c17d0e498ea287d1a386/raw/7def9ef4234ddf0bc82f855ad67dac8b971852ef/loans2.txt'
 url3 = 'https://gist.githubusercontent.com/aakashns/257f6e6c8719c17d0e498ea287d1a386/raw/7def9ef4234ddf0bc82f855ad67dac8b971852ef/loans3.txt'
 
-urlretrieve(url1, './myData/loans1.txt')
+# urlretrieve(url1, './myData/loans1.txt')
+# urlretrieve(url2, './myData/loans2.txt')
+# urlretrieve(url3, './myData/loans3.txt')
+
+print("Verify if file exists in myData")
+print(os.listdir("./myData"))
+print("\n")
+
+#open the file using with method
+with open("./myData/loans2.txt", mode='r') as file2:
+    #read a file line by line
+    file_contents2 = file2.readlines()
+    print(file_contents2)
+
+
+
+
+
